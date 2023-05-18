b5f7eb33e0f25e26b19c3e10d6407596d3d9925d
Mahedi3110 <shuvo@shuvo>
2023-05-18 07:14:14 +0600

    mongodb configuration added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 7000;
@@ -7,6 +8,39 @@ const port = process.env.PORT || 7000;
 //middleware
 app.use(cors())
 app.use(express.json())
+
+// mongoDB
+const uri = `mongodb+srv://${process.env.USER}:${process.env.PASS}@cluster0.lpihfko.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        await client.connect();
+
+        const components = client.db("gamingComponents").collection("products");
+
+        app.get('/category', async (req, res) => {
+            const result = await components.find().toArray();
+            res.send(result);
+        })
+
+
+
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
 
 
 
